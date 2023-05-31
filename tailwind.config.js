0a0f4fdd9fbe37d92720c64caae2b0f3e0ba1a83
sastra12 <anonymousonly594@gmail.com>
2023-05-31 21:33:35 +0700

    install tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  theme: {
+    fontFamily: {
+      poppins: ['Poppins', 'sans-serif']
+    },
+    extend: {
+      colors: {
+        primary: '#264653',
+        secondary: '#2a9d8f'
+      }
+    }
+  },
+  plugins: []
+}
