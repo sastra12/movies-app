0a5db42c4143d48a8a9f18ca95f53d7cd768885c
sastra12 <anonymousonly594@gmail.com>
2023-06-16 11:33:55 +0700

    edit DetailMovie Pages

--- src/pages/DetailMovie.vue
+++ src/pages/DetailMovie.vue
@@ -1,41 +1,37 @@
 <template>
   <default-container>
-    <div class="flex flex-col sm:flex-row sm:gap-x-10 sm:mt-4">
-      <div class="sm:w-3/12">
+    <div class="flex flex-col sm:flex-row sm:gap-x-5 sm:mt-4">
+      <div class="sm:w-2/5 lg:w-3/12">
         <img src="@/assets/img/banner.jpg" class="rounded-md h-72 sm:h-96 w-full bg-cover" alt="" />
       </div>
-      <div class="sm:w-9/12 pl-3">
+      <div class="sm:w-3/5 lg:w-9/12 pl-3">
         <h1 class="mt-3 font-poppins text-lg font-semibold text-secondary">
-          Spider-Man: Across the Spider-Verse (2023)
+          {{ detailMovie.title }}
         </h1>
         <!-- Tagline -->
-        <h4 class="font-poppins text-base mt-1 text-secondary2 font-semibold italic">
-          The end of the road begins.
+        <h4 class="font-poppins text-base text-secondary2 font-semibold italic" v-if="hidden">
+          {{ detailMovie.tagline }}
         </h4>
-        <span class="text-sm font-poppins opacity-75">Action, Comedy, Animation</span>
+        <span class="font-light">{{ getGenreNames }}</span>
+
         <!-- Overview -->
-        <h3 class="font-poppins mt-5 font-semibold text-lg">Overview</h3>
-        <p class="font-poppins text-sm">
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugit quod rerum, id autem ipsum
-          libero amet quae, quam voluptate excepturi error earum eos dolores veniam non! Quae
-          repellendus vel sint, soluta sapiente molestias aperiam ipsum molestiae beatae repudiandae
-          odit necessitatibus a delectus, modi distinctio dolore incidunt quaerat veritatis rem, in
-          enim voluptate? Minus veritatis dolores fuga tempore, inventore vel quo. Vel est,
-          doloribus delectus architecto laborum at obcaecati illum incidunt eum dolorem recusandae
-          pariatur, facere error deserunt quia aperiam repellendus!
+        <h3 class="font-poppins mt-2 font-semibold text-lg">Overview</h3>
+        <p class="font-poppins text-sm text-justify">
+          {{ detailMovie.overview }}
         </p>
 
         <!-- Status -->
         <div>
           <div class="mt-5 flex flex-col sm:flex-row sm:gap-x-4 border-b pb-4">
             <p class="font-poppins text-base">
-              Status : <span class="font-poppins opacity-80">Released</span>
+              Status : <span class="font-poppins opacity-80">{{ detailMovie.status }}</span>
             </p>
             <p class="font-poppins text-base">
-              Release Date : <span class="font-poppins opacity-80">23 Mei 2023</span>
+              Release Date :
+              <span class="font-poppins opacity-80">{{ detailMovie.release_date }}</span>
             </p>
             <p class="font-poppins text-base">
-              Runtime : <span class="font-poppins opacity-80">2h 22 m</span>
+              Runtime : <span class="font-poppins opacity-80">{{ convertRuntime }}</span>
             </p>
           </div>
           <div class="border-b pb-4 my-4">
@@ -45,10 +41,8 @@
           </div>
           <div class="border-b pb-4">
             <p class="font-poppins text-base">
-              Writer :
-              <span class="font-poppins opacity-80"
-                >Justin Lin, Justin Lin, Dan Mazeau, Dan Mazeau, Zach Dean</span
-              >
+              Budget :
+              <span class="font-poppins opacity-80">{{ detailMovie.budget }}</span>
             </p>
           </div>
         </div>
@@ -59,4 +53,45 @@
 
 <script setup>
 import DefaultContainer from '@/components/Layouts/DefaultContainer.vue'
+import { computed, inject, onMounted, ref } from 'vue'
+import { useRoute } from 'vue-router'
+
+const axiosInstance = inject('$axios')
+const route = useRoute()
+const detailMovie = ref([])
+
+const getDetailMovie = async () => {
+  try {
+    const response = await axiosInstance(
+      `movie/` + route.params.id + `&language=en-US&append_to_response=credits`
+    )
+    detailMovie.value = response.data
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+const hidden = computed(() => {
+  if (detailMovie.tagline == '') {
+    return false
+  }
+  return true
+})
+
+const convertRuntime = computed(() => {
+  const hours = Math.floor(detailMovie.value.runtime / 60)
+  const minutes = detailMovie.value.runtime % 60
+  return `${hours}h ${minutes}m`
+})
+
+const getGenreNames = computed(() => {
+  const genres = detailMovie.value.genres
+  if (Array.isArray(genres)) {
+    return genres.map((genre) => genre.name).join(',')
+  }
+})
+
+onMounted(async () => {
+  getDetailMovie()
+})
 </script>
