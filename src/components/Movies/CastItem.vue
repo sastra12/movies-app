3bffa67a5463768ebb01d12ad42802f823190baa
sastra12 <anonymousonly594@gmail.com>
2023-06-17 21:27:33 +0700

    make CastItem

---
+++ src/components/Movies/CastItem.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="border text-center pb-2">
+    <img :src="poster_path" alt="" class="bg-cover rounded-sm" />
+    <p class="text-lg font-poppins font-semibold mt-3">{{ item.name }}</p>
+    <span class="font-poppins font-light text-base">{{ item.character }}</span>
+  </div>
+</template>
+
+<script setup>
+import { computed } from 'vue'
+
+const props = defineProps({
+  item: {
+    required: true,
+    type: Object
+  }
+})
+
+const poster_path = computed(() => {
+  if (props.item.profile_path) {
+    return 'https://image.tmdb.org/t/p/w500/' + props.item.profile_path
+  } else if (props.item.profile_path == null) {
+    return 'https://via.placeholder.com/300x450'
+  }
+})
+</script>
--- src/pages/DetailMovie.vue
+++ src/pages/DetailMovie.vue
@@ -2,7 +2,7 @@
   <default-container>
     <div class="flex flex-col sm:flex-row sm:gap-x-5 sm:mt-4">
       <div class="sm:w-2/5 lg:w-3/12">
-        <img src="@/assets/img/banner.jpg" class="rounded-md h-72 sm:h-96 w-full bg-cover" alt="" />
+        <img :src="poster_path" class="rounded-md h-72 sm:h-96 w-full bg-cover" alt="" />
       </div>
       <div class="sm:w-3/5 lg:w-9/12 pl-3">
         <h1 class="mt-3 font-poppins text-lg font-semibold text-secondary">
@@ -48,50 +48,118 @@
         </div>
       </div>
     </div>
+    <div class="mt-4 sm:mt-8">
+      <h1 class="text-lg font-bold font-poppins">Top Cast</h1>
+      <Swiper
+        class="p-4 mt-2 sm:mt-8"
+        :modules="modules"
+        :slidesPerView="1"
+        :freeMode="true"
+        :pagination="{
+          type: 'progressbar'
+        }"
+        :breakpoints="{
+          '@0.00': {
+            slidesPerView: 2,
+            spaceBetween: 10
+          },
+          '@0.75': {
+            slidesPerView: 3,
+            spaceBetween: 10
+          },
+          '@1.00': {
+            slidesPerView: 5,
+            spaceBetween: 10
+          },
+          '@1.50': {
+            slidesPerView: 8,
+            spaceBetween: 10
+          }
+        }"
+      >
+        <Swiper-Slide v-for="item in casts" :key="item" class="pt-2 sm:pt-3">
+          <cast-item :item="item" />
+        </Swiper-Slide>
+      </Swiper>
+    </div>
   </default-container>
 </template>
 
-<script setup>
+<script>
+import { Swiper, SwiperSlide } from 'swiper/vue'
+// import required modules
+import { Pagination, FreeMode, Autoplay } from 'swiper'
 import DefaultContainer from '@/components/Layouts/DefaultContainer.vue'
+import CastItem from '@/components/Movies/CastItem.vue'
 import { computed, inject, onMounted, ref } from 'vue'
 import { useRoute } from 'vue-router'
 
-const axiosInstance = inject('$axios')
-const route = useRoute()
-const detailMovie = ref([])
+export default {
+  components: {
+    DefaultContainer,
+    Swiper,
+    SwiperSlide,
+    CastItem
+  },
+  setup() {
+    const axiosInstance = inject('$axios')
+    const route = useRoute()
+    const detailMovie = ref([])
+    const casts = ref([])
 
-const getDetailMovie = async () => {
-  try {
-    const response = await axiosInstance(
-      `movie/` + route.params.id + `&language=en-US&append_to_response=credits`
-    )
-    detailMovie.value = response.data
-  } catch (error) {
-    console.log(error)
+    const getDetailMovie = async () => {
+      try {
+        const response = await axiosInstance(
+          `movie/` + route.params.id + `?language=en-US&append_to_response=credits`
+        )
+        detailMovie.value = response.data
+        casts.value = response.data.credits.cast
+      } catch (error) {
+        console.log(error)
+      }
+    }
+
+    const hidden = computed(() => {
+      if (detailMovie.tagline == '') {
+        return false
+      }
+      return true
+    })
+
+    const convertRuntime = computed(() => {
+      const hours = Math.floor(detailMovie.value.runtime / 60)
+      const minutes = detailMovie.value.runtime % 60
+      return `${hours}h ${minutes}m`
+    })
+
+    const getGenreNames = computed(() => {
+      const genres = detailMovie.value.genres
+      if (Array.isArray(genres)) {
+        return genres.map((genre) => genre.name).join(',')
+      }
+    })
+
+    const poster_path = computed(() => {
+      if (detailMovie.value) {
+        return 'https://image.tmdb.org/t/p/w500/' + detailMovie.value.poster_path
+      } else if (detailMovie.value.poster_path == null) {
+        return 'https://via.placeholder.com/300x450'
+      }
+    })
+
+    onMounted(async () => {
+      getDetailMovie()
+    })
+
+    return {
+      poster_path,
+      getGenreNames,
+      casts,
+      convertRuntime,
+      modules: [Pagination, FreeMode, Autoplay],
+      hidden,
+      detailMovie
+    }
   }
 }
-
-const hidden = computed(() => {
-  if (detailMovie.tagline == '') {
-    return false
-  }
-  return true
-})
-
-const convertRuntime = computed(() => {
-  const hours = Math.floor(detailMovie.value.runtime / 60)
-  const minutes = detailMovie.value.runtime % 60
-  return `${hours}h ${minutes}m`
-})
-
-const getGenreNames = computed(() => {
-  const genres = detailMovie.value.genres
-  if (Array.isArray(genres)) {
-    return genres.map((genre) => genre.name).join(',')
-  }
-})
-
-onMounted(async () => {
-  getDetailMovie()
-})
 </script>
