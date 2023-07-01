8a8ec9c66f5ee41057ba5c77cbfe1b1d2967b474
sastra12 <anonymousonly594@gmail.com>
2023-07-01 18:00:43 +0700

    edit footer

--- src/service/axios.js
+++ src/service/axios.js
@@ -1,11 +1,9 @@
 import axios from 'axios'
 
-const token = import.meta.env.VITE_APP_TOKEN
-
 const axiosInstance = axios.create({
   baseURL: 'https://api.themoviedb.org/3/',
   headers: {
-    Authorization: `Bearer ${token}`
+    Authorization: `Bearer ${import.meta.env.VITE_APP_TOKEN}`
   }
 })
 
