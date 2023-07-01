996e9f869a559cdcd7edbe6868d07a0b4f3044f4
sastra12 <anonymousonly594@gmail.com>
2023-07-01 19:00:16 +0700

    add file redirects

--- src/components/SearchNotFound.vue
+++ src/components/SearchNotFound.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex flex-col items-center not-found">
     <img src="@/assets/search-not-found.svg" alt="" class="w-[350px] h-auto mx-auto" />
-    <h4 class="mx-auto font-poppins font-bold text-xl mt-4">Not results found</h4>
+    <h4 class="mx-auto font-poppins font-bold text-xl mt-4">Search not found</h4>
   </div>
 </template>
 
