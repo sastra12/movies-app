0dcdf1679db582fc20532aa0dc0da11cd1f9a15f
sastra12 <anonymousonly594@gmail.com>
2023-07-02 09:53:12 +0700

    edit font poppins

--- src/components/Discover/SearchResultItem.vue
+++ src/components/Discover/SearchResultItem.vue
@@ -11,7 +11,7 @@
       <h1 v-else class="mt-3 font-poppins text-base font-semibold text-secondary">
         {{ props.item.original_name }}
       </h1>
-      <span>{{ genreTypeName(props.item.genre_ids) }}</span>
+      <span class="text-sm font-poppins">{{ genreTypeName(props.item.genre_ids) }}</span>
       <div class="flex items-center justify-between">
         <span
           v-if="props.item.hasOwnProperty('release_date')"
@@ -34,7 +34,9 @@
               d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z"
             />
           </svg>
-          <span class="text-sm">{{ Math.round(props.item.vote_average * 10) / 10 }}</span>
+          <span class="text-sm font-poppins">{{
+            Math.round(props.item.vote_average * 10) / 10
+          }}</span>
         </div>
       </div>
     </router-link>
