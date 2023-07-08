b6b0ce6fb97529080ab46fef3773c83cd790fe9a
sastra12 <anonymousonly594@gmail.com>
2023-07-08 15:20:55 +0700

    edit width navbar

--- src/components/Reusable/BaseModel.vue
+++ src/components/Reusable/BaseModel.vue
@@ -3,7 +3,7 @@
     v-show="modalActive"
     class="absolute w-full bg-black bg-opacity-30 h-screen top-0 left-0 flex justify-center px-8 z-50"
   >
-    <div v-if="modalActive" class="p-4 bg-white self-start mt-32 w-2/5">
+    <div v-if="modalActive" class="p-4 bg-white self-start mt-32 w-full sm:w-1/2">
       <slot />
       <button
         class="bg-red-500 mt-4 rounded-sm bg-weather-primary py-2 px-4"
