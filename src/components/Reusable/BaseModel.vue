1569ceb73085351fe502c88e9ae76a3936bf0e80
sastra12 <anonymousonly594@gmail.com>
2023-07-08 15:44:53 +0700

    fix navbar

--- src/components/Reusable/BaseModel.vue
+++ src/components/Reusable/BaseModel.vue
@@ -3,12 +3,9 @@
     v-show="modalActive"
     class="absolute w-full bg-black bg-opacity-30 h-screen top-0 left-0 flex justify-center px-8 z-50"
   >
-    <div v-if="modalActive" class="p-4 bg-white self-start mt-32 w-full sm:w-4/5">
+    <div v-if="modalActive" class="p-4 bg-white self-start mt-32 w-full sm:w-2/4">
       <slot />
-      <button
-        class="bg-red-500 mt-4 rounded-sm bg-weather-primary py-2 px-4"
-        @click="$emit('close-modal')"
-      >
+      <button class="bg-red-500 mt-4 rounded-sm py-2 px-4 text-white" @click="$emit('close-modal')">
         Close
       </button>
     </div>
